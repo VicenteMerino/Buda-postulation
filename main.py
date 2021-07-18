3d04f2d7d8f6053d746832b1cf223bab180cca54
VicenteMerino <vamerino@uc.cl>
2021-07-18 00:57:35 -0400

    Add BFS solution, network structure and initial tests

---
+++ main.py
@@ -0,0 +1,69 @@
+from sys import argv
+import os
+import json
+from metro import Metro
+
+if __name__ == "__main__":
+    if len(argv) < 4 or len(argv) > 5:
+        raise Exception(
+            (
+                "The format to use the program is: "
+                "python main.py <network_file> <origin_station> "
+                "<end_station> <bus (optional)>"
+            )
+        )
+    network_file = argv[1]
+    origin_station = argv[2]
+    end_station = argv[3]
+    bus = argv[4] if len(argv) > 4 else None
+    if bus not in ("red", "green", None):
+        raise ValueError(
+            "Bus color value incorrect (must be 'red', 'green', or None type)."
+        )
+    if not os.path.exists(network_file):
+        raise Exception(f"Path {network_file} don't exist.")
+
+    with open(network_file) as file:
+        network_json = json.load(file)
+
+    stations = network_json["stations"]
+    edges = network_json["edges"]
+
+    metro = Metro()
+
+    for station in stations:
+        station_name = station[0]
+        station_color = station[1]
+        metro.add_station(station_name, station_color)
+
+    for edge in edges:
+        station_1 = edge[0]
+        station_2 = edge[1]
+        metro.add_edge(station_1, station_2)
+
+    if origin_station not in metro.stations:
+        raise ValueError(
+            (
+                f"Station {origin_station} is not on "
+                f"the network specified in {network_file}"
+            )
+        )
+
+    if end_station not in metro.stations:
+        raise ValueError(
+            (
+                f"Station {end_station} is not on "
+                f"the network specified in {network_file}"
+            )
+        )
+
+    (are_connected, path, path_length) = metro.solve_shortest_route(
+        bus, origin_station, end_station
+    )
+
+    if are_connected:
+        print(
+            f"The shortest path is:\n{'->'.join(path)}\nWith length: {path_length}"
+        )
+    else:
+        print(f"The nodes are not connected because of: '{path}'")
---
+++ metro.py
@@ -0,0 +1,134 @@
+class Metro:
+    def __init__(self):
+        self.stations = []
+        self.colors = []
+        self.edges = dict()
+
+    def add_station(self, station, color=None):
+        if color not in (None, "red", "green"):
+            raise ValueError(
+                "Station color value incorrect "
+                "(must be 'red', 'green', or None type)."
+            )
+        if not isinstance(station, str):
+            raise TypeError("Station must be a string.")
+
+        if station in self.stations:
+            raise ValueError("Station already in the network")
+
+        self.stations.append(station)
+        self.colors.append(color)
+
+    def add_edge(self, station_1, station_2):
+        if station_1 not in self.stations:
+            raise ValueError(f"Station {station_1} has not been added")
+        if station_2 not in self.stations:
+            raise ValueError(f"Station {station_2} has not been added")
+        if station_1 not in self.edges:
+            self.edges[station_1] = []
+        self.edges[station_1].append(station_2)
+        if station_2 not in self.edges:
+            self.edges[station_2] = []
+        self.edges[station_2].append(station_1)
+
+    def solve_shortest_route(self, bus, origin, end):
+        origin_index = self.stations.index(origin)
+        end_index = self.stations.index(end)
+
+        if (
+            self.colors[origin_index] == "green"
+            and self.colors[end_index] == "red"
+        ) or (
+            self.colors[end_index] == "red"
+            and self.colors[origin_index] == "green"
+        ):
+            return (
+                False,
+                f"origin and end stations of different color ({self.colors[origin_index]} & {self.colors[end_index]})",
+                None,
+            )
+        if (
+            bus
+            and self.colors[origin_index]
+            and bus != self.colors[origin_index]
+        ):
+            return (
+                False,
+                f"the origin station and the bus has different colors ({self.colors[origin_index]} & {bus})",
+                None,
+            )
+        if bus and self.colors[end_index] and bus != self.colors[end_index]:
+            return (
+                False,
+                f"the end station and the bus has different colors ({self.colors[end_index]} & {bus})",
+                None,
+            )
+        
+        if origin == end:
+            return (
+                True, [origin], 0
+            )
+
+        # Now we are ready to execute bfs
+        N = len(self.stations)
+        queue = []
+        visited = [False for _ in range(N)]
+        visited[self.stations.index(origin)] = True
+        distance = [10000000 for _ in range(N)]
+        distance[self.stations.index(origin)] = 0
+        pred = [-1 for _ in range(N)]
+        queue.append(origin)
+
+        while len(queue):
+            current_station = queue.pop(0)
+            current_station_index = self.stations.index(current_station)
+            for i in range(len(self.edges[current_station])):
+                next_station_index = self.stations.index(
+                    self.edges[current_station][i]
+                )
+
+                if not visited[next_station_index]:
+                    visited[next_station_index] = True
+                    if (
+                        self.colors[next_station_index] == "green"
+                        and bus == "red"
+                    ) or (
+                        self.colors[next_station_index] == "red"
+                        and bus == "green"
+                    ):
+                        distance[next_station_index] = distance[
+                            current_station_index
+                        ]
+                    else:
+                        distance[next_station_index] = (
+                            distance[current_station_index]
+                        ) + 1
+                    pred[next_station_index] = current_station
+                    queue.append(self.edges[current_station][i])
+
+                    if self.edges[current_station][i] == end:
+                        path = []
+                        prev_station = end
+                        prev_station_index = self.stations.index(end)
+                        path.append(prev_station)
+
+                        path_built = False
+
+                        while not path_built:
+                            prev_station = pred[prev_station_index]
+                            prev_station_index = self.stations.index(
+                                prev_station
+                            )
+                            if (
+                                not self.colors[prev_station_index]
+                                or not bus
+                                or self.colors[prev_station_index] == bus
+                            ):
+                                path.append(prev_station)
+
+                            if origin == prev_station:
+                                path_built = True
+
+                        return True, list(reversed(path)), len(path) - 1
+        return False, "Network architecture", None
+
